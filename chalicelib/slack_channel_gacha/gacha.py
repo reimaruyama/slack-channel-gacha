6421a2f72e85c17f623b8f66c23771d9b0889964
Rei Maruyama <rei.maruyama@serverworks.co.jp>
2020-03-20 23:29:31 +0900

    style: tabをspaceに統一した

--- chalicelib/slack_channel_gacha/gacha.py
+++ chalicelib/slack_channel_gacha/gacha.py
@@ -5,47 +5,47 @@ from .channel import Channel, ChannelId, ChannelPurpose
 from .client import Client
 
 class Gacha:
-	def __init__(self):
-		self.__client = Client(token=env['SLACK_BOT_USER_TOKEN'], channel=env["SLACK_OUTPUT_CHANNEL"])
+    def __init__(self):
+        self.__client = Client(token=env['SLACK_BOT_USER_TOKEN'], channel=env["SLACK_OUTPUT_CHANNEL"])
 
-	def play(self):
-		channels = self.__client.fetch_channels()
-		selected_channel = self.select(channels)
-		message = self.build_message(selected_channel)
-		self.__client.notify(message)
+    def play(self):
+        channels = self.__client.fetch_channels()
+        selected_channel = self.select(channels)
+        message = self.build_message(selected_channel)
+        self.__client.notify(message)
 
-	def select(self, channels):
-		"""
-		引数で与えられたチャンネルから一つ選択します
-		"""
-		selected = random.sample(channels, 1)[0]
-		return self.convert_channel_object(selected)
-
-
-	def convert_channel_object(self, channel):
-		"""
-		引数で与えられたslack APIの返り値のchannnelオブジェクト(dict)
-		をGachaedChannelオブジェクトにして返します。
-		"""
-		return Channel(
-			id = ChannelId(channel["id"]),
-			purpose = ChannelPurpose(channel["purpose"]["value"])
-		)
+    def select(self, channels):
+        """
+        引数で与えられたチャンネルから一つ選択します
+        """
+        selected = random.sample(channels, 1)[0]
+        return self.convert_channel_object(selected)
 
 
-	def build_message(self, channel):
-		"""
+    def convert_channel_object(self, channel):
+        """
+        引数で与えられたslack APIの返り値のchannnelオブジェクト(dict)
+        をGachaedChannelオブジェクトにして返します。
+        """
+        return Channel(
+            id = ChannelId(channel["id"]),
+            purpose = ChannelPurpose(channel["purpose"]["value"])
+        )
+
+
+    def build_message(self, channel):
+        """
         Slackに投稿する形式の文字列を返します。
         チャンネルの説明が空文字列だった場合は`未設定`と表示します。
         """
-		message = f"本日のチャンネルガチャ\nチャンネル: <#{str(channel.id)}>\n説明: {str(channel.purpose)}"
-		return message
+        message = f"本日のチャンネルガチャ\nチャンネル: <#{str(channel.id)}>\n説明: {str(channel.purpose)}"
+        return message
 
 
 def main():
-	gacha = Gacha()
-	gacha.play()
+    gacha = Gacha()
+    gacha.play()
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- tests/test_slack_channel_gacha.py
+++ tests/test_slack_channel_gacha.py
@@ -10,92 +10,92 @@ from chalicelib.slack_channel_gacha.channel import ChannelPurpose, ChannelId, Ch
 from chalicelib.slack_channel_gacha.gacha import Gacha
 
 def test_channel_id_is_started_with_big_c():
-	"""
-	チャンネルIDは大文字のCで開始していないとエラーになる
-	"""
-	with pytest.raises(AttributeError):
-		ChannelId("AAAAA")
+    """
+    チャンネルIDは大文字のCで開始していないとエラーになる
+    """
+    with pytest.raises(AttributeError):
+        ChannelId("AAAAA")
 
 
 def test_channel_id_raise_error_if_len_is_ten():
-	"""
-	チャンネルIDは10文字の場合エラーになる
-	"""
-	with pytest.raises(AttributeError):
-		ChannelId("C123456789")
+    """
+    チャンネルIDは10文字の場合エラーになる
+    """
+    with pytest.raises(AttributeError):
+        ChannelId("C123456789")
 
 
 def test_channel_id_raise_error_if_len_is_eight():
-	"""
-	チャンネルIDは8文字の場合エラーになる
-	"""
-	with pytest.raises(AttributeError):
-		ChannelId("C1234567")
+    """
+    チャンネルIDは8文字の場合エラーになる
+    """
+    with pytest.raises(AttributeError):
+        ChannelId("C1234567")
 
 
 def test_channel_id_is_alphabet_or_num_character():
-	"""
-	チャンネルIDに英数字以外が含まれる場合、エラーになる
-	"""
-	with pytest.raises(AttributeError):
-		ChannelId("C12345Aあ亜")
+    """
+    チャンネルIDに英数字以外が含まれる場合、エラーになる
+    """
+    with pytest.raises(AttributeError):
+        ChannelId("C12345Aあ亜")
 
 def test_channel_id_is_valid():
-	"""
-	妥当なチャンネルIDを格納できる
-	"""
-	channel_id = ChannelId("C2740KJ1K")
-	assert channel_id.value == "C2740KJ1K"
+    """
+    妥当なチャンネルIDを格納できる
+    """
+    channel_id = ChannelId("C2740KJ1K")
+    assert channel_id.value == "C2740KJ1K"
 
 
 def channel_purpose_length_is_shorter_than_251():
-	"""
-	チャンネル説明が251字以上の場合エラーになる
-	"""
-	purpose = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(251)])
-	with pytest.raises(AssertionError):
-		ChannelPurpose(purpose)
+    """
+    チャンネル説明が251字以上の場合エラーになる
+    """
+    purpose = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(251)])
+    with pytest.raises(AssertionError):
+        ChannelPurpose(purpose)
 
 
 def channel_purpose_length_is_valid_in_250_char():
-	"""
-	チャンネル説明が250字以内の場合エラーにならない
-	"""
-	purpose = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(250)])
-	channel_purpose = ChannelPurpose(purpose)
-	assert channel_purpose.value == purpose
+    """
+    チャンネル説明が250字以内の場合エラーにならない
+    """
+    purpose = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(250)])
+    channel_purpose = ChannelPurpose(purpose)
+    assert channel_purpose.value == purpose
 
 
 def test_channel_object():
-	channel = Channel(
-		id=ChannelId("C2740KJ1K"),
-		purpose=ChannelPurpose("チャンネル説明")
-	)
-	assert channel.id.value == "C2740KJ1K"
-	assert channel.purpose.value == "チャンネル説明"
+    channel = Channel(
+        id=ChannelId("C2740KJ1K"),
+        purpose=ChannelPurpose("チャンネル説明")
+    )
+    assert channel.id.value == "C2740KJ1K"
+    assert channel.purpose.value == "チャンネル説明"
 
 def test_channel_object_for_post_string():
-	channel = Channel(
-		id=ChannelId("C2740KJ1K"),
-		purpose=ChannelPurpose("チャンネル説明")
-	)
-	gacha = Gacha()
-	post_text = gacha.build_message(channel)
+    channel = Channel(
+        id=ChannelId("C2740KJ1K"),
+        purpose=ChannelPurpose("チャンネル説明")
+    )
+    gacha = Gacha()
+    post_text = gacha.build_message(channel)
 
-	assert post_text == "本日のチャンネルガチャ\nチャンネル: <#C2740KJ1K>\n説明: チャンネル説明"
+    assert post_text == "本日のチャンネルガチャ\nチャンネル: <#C2740KJ1K>\n説明: チャンネル説明"
 
 def test_channel_object_purpose_blank():
-	"""
-	チャンネルの説明が空文字列のみの場合は未設定と表示される
-	"""
-	channel = Channel(
-		id=ChannelId("C2740KJ1K"),
-		purpose=ChannelPurpose("")
-	)
-	gacha = Gacha()
-	post_text = gacha.build_message(channel)
+    """
+    チャンネルの説明が空文字列のみの場合は未設定と表示される
+    """
+    channel = Channel(
+        id=ChannelId("C2740KJ1K"),
+        purpose=ChannelPurpose("")
+    )
+    gacha = Gacha()
+    post_text = gacha.build_message(channel)
 
-	assert post_text == "本日のチャンネルガチャ\nチャンネル: <#C2740KJ1K>\n説明: 未設定"
+    assert post_text == "本日のチャンネルガチャ\nチャンネル: <#C2740KJ1K>\n説明: 未設定"
 
 
 # TODO: mockを使ってslack APIのテストをかく
